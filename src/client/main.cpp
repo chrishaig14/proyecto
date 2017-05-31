343dd2ee4271eb1903a29dd7c3759a29c0a075af
chris <chrishaig14@gmail.com>
2017-05-30 22:27:40 -0300

    Código de cliente y manager andando. Hay que hacer la persistencia de la tabla.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,4 +2,104 @@
 // Created by chris on 30/05/17.
 //
 
-int main() {}
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include "../msgqueue/Cola.h"
+
+#define REQUEST_MSG 1;
+#define ADD 1
+#define GET 2
+
+struct record {
+    char name[61];
+    char address[120];
+    char phone[13];
+};
+
+struct request {
+    long mtype;
+    int client;
+    int req_type;
+    struct record rec;
+};
+
+void handle_add(Cola<struct request> &msg_queue) {
+    struct request req;
+    req.mtype = REQUEST_MSG;
+    req.req_type = ADD;
+    req.client = getpid();
+    struct record rec;
+    std::string line;
+    std::cout << "Name:";
+    do {
+        std::getline(std::cin, line);
+    } while (line.size() > 61);
+
+    strncpy(rec.name, line.c_str(), 61);
+
+    std::cout << "Address:";
+    do {
+        std::getline(std::cin, line);
+    } while (line.size() > 120);
+    strncpy(rec.address, line.c_str(), 120);
+
+    std::cout << "Phone:";
+    do {
+        std::getline(std::cin, line);
+    } while (line.size() > 13);
+    strncpy(rec.phone, line.c_str(), 13);
+    req.rec = rec;
+    msg_queue.escribir(req);
+    std::cout << "[client] waiting for server's response" << std::endl;
+    msg_queue.leer(getpid(), &req);
+    std::cout << "[client]" << req.req_type << std::endl;
+}
+
+void handle_get(Cola<struct request> &msg_queue) {
+    struct request req;
+    req.mtype = REQUEST_MSG;
+    req.req_type = GET;
+    req.client = getpid();
+    std::string line;
+    std::cout << "Name:";
+    do {
+        std::getline(std::cin, line);
+    } while (line.size() > 61);
+
+    strncpy(req.rec.name, line.c_str(), 61);
+    std::cout << "GET " << req.rec.name << std::endl;
+    msg_queue.escribir(req);
+    std::cout << "[client] waiting for server's response" << std::endl;
+    msg_queue.leer(getpid(), &req);
+    std::cout << "Name:" << req.rec.name << std::endl;
+    std::cout << "Address:" << req.rec.address << std::endl;
+    std::cout << "Phone:" << req.rec.phone << std::endl;
+}
+
+
+int main() {
+    Cola<struct request> msg_queue("/bin/bash", 'a');
+    char c = '0';
+    while (true) {
+        std::cout << "1. Add record" << std::endl;
+        std::cout << "2. Get record by name" << std::endl;
+        c = (char) getchar();
+        while (c != '1' && c != '2' && c != 'q') {
+            c = (char) getchar();
+        }
+        if (c == 'q') {
+            break;
+        }
+        std::cin.ignore();
+        if (c == '1') {
+            handle_add(msg_queue);
+        }
+        if (c == '2') {
+            handle_get(msg_queue);
+        }
+
+    }
+
+
+}
--- src/manager/main.cpp
+++ src/manager/main.cpp
@@ -2,4 +2,63 @@
 // Created by chris on 30/05/17.
 //
 
-int main() {}
+#include <iostream>
+#include <map>
+#include "../msgqueue/Cola.h"
+
+#define REQUEST_MSG 1
+#define ADD 1
+#define GET 2
+
+struct record {
+    char name[61];
+    char address[120];
+    char phone[13];
+};
+
+class Database {
+    std::map<std::string, struct record> data;
+public:
+    struct record get(std::string name) {
+        return data[name];
+    }
+
+    void add(struct record rec) {
+        data[rec.name] = rec;
+    }
+};
+
+
+struct request {
+    long mtype;
+    int client;
+    int req_type;
+    struct record rec;
+};
+
+struct request handle_request(Database &db, struct request req) {
+
+    if (req.req_type == ADD) {
+        std::cout << "ADD " << req.rec.name << " , " << req.rec.address << " , " << req.rec.phone << std::endl;
+        db.add(req.rec);
+    }
+    if (req.req_type == GET) {
+        std::cout << "GET " << req.rec.name << std::endl;
+        req.rec = db.get(req.rec.name);
+    }
+    req.mtype = req.client;
+    return req;
+}
+
+int main() {
+    Cola<struct request> msg_queue("/bin/bash", 'a');
+    Database db;
+    while (true) {
+        struct request req;
+        std::cout << "[server] waiting for request" << std::endl;
+        msg_queue.leer(REQUEST_MSG, &req);
+        std::cout << "[server] read " << req.mtype << std::endl;
+        struct request response = handle_request(db, req);
+        msg_queue.escribir(response);
+    }
+}
